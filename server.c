317728e7c3c93466c92ffcf8557f78ddaf4ae52b
relefebvre <remi.ladonch@gmail.com>
2014-02-06 16:13:21 +0100

    Modif

--- server.c
+++ server.c
@@ -9,20 +9,30 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-
-//char *path;
 
 typedef struct {
 	int socket; 
 } argTh;
+
+typedef struct {
+	char *version;
+	char *codeRetour;
+	char *filename;
+} enTete;
 
 void *codeFrere(void *arg)
 {
 	argTh *val = arg;
 	char buffer[2048];	
 	char *pathName;
-	int file;
+	int file,nbSend;
+	enTete tete;
+	//struct msghdr enTeteHtml;
+	//struct iovec msg; 
+	char *reponse;
 
 	if (read(val->socket,buffer,sizeof(buffer)) < 0 )
 	{
@@ -30,11 +40,75 @@ void *codeFrere(void *arg)
 		exit(1);
 	}
 	
-	printf("Buffer : %s\n",buffer);
-	//file = open(buffer,"r");
+	parser(&tete,buffer);
+	printf("File : %s\n",tete.filename);
+	printf("Version : %s\n",tete.version);
+	file = openFile(&tete);
+
+	/*Tentage de truc pour sendmsg()*/
+	/*msg.iov_base = &tete;
+	msg.iov_len = strlen(tete.version)+strlen(tete.codeRetour);
+	enTeteHtml.msg_iov = &msg;	
+	*/
+
+	reponse = (char *)malloc(strlen(tete.version)+strlen(tete.codeRetour)+2);
+	if (reponse == NULL)
+	{
+		close(val->socket);
+		printf("Problème malloc\n");
+		exit(errno);
+	}
+
+	nbSend = write(val->socket,tete.version,strlen(tete.version)-1);
+	nbSend = write(val->socket,tete.codeRetour,strlen(tete.codeRetour));
+	strcpy(buffer,"\n");
+	nbSend = write(val->socket,buffer,strlen(buffer));
+	//while((nbSend = read(file,buffer,sizeof(buffer))) > 0)
+	//	nbSend = write(val->socket,buffer,nbSend); 
+
+
+	printf("NbLu : %d\n",nbSend);
+	
+	if (file == 0)
+	{
+	}
 		
+	close(val->socket);
 	free(val);
 	pthread_detach(pthread_self());
+	pthread_exit(NULL);
+}
+
+int openFile(enTete *tete)
+{
+	int ret;
+	ret = open(tete->filename,O_RDONLY);
+	
+	if (ret >= 0)
+		tete->codeRetour = strdup(" 200 OK\r\n");
+	else
+		tete->codeRetour = strdup(" 404 Not Found\r\n");
+	return ret;
+}
+
+void parser(enTete *tete, char *http)
+{
+	char *token=NULL;
+	token = strtok(http," ");
+	while(token)
+	{
+		switch (token[0]) {
+			case '/' :
+				tete->filename = strdup(&token[1]);
+				break;
+			case 'H' :
+				token = strtok(token,"\n");
+				tete->version = strtok(token,"\n");
+				break;
+		}
+
+		token = strtok(NULL," ");
+	}
 }
 
 void setPath(char **path, char *src)
@@ -54,11 +128,10 @@ int main(int argc, char** argv)
 	char *path=NULL;
 	unsigned long maxTh = 20;
 	unsigned debug = 0;
-	argTh *arg;	
         int fd, cfd;
         struct sockaddr_in sin, cin;
         unsigned int port=8080;
-        char hostname[]="0.0.0.0";
+        char hostname[]="127.0.0.1";
         socklen_t taille;
         struct hostent *hostinfo;
 
@@ -139,9 +212,10 @@ int main(int argc, char** argv)
 	while ((cfd = accept(fd,(struct sockaddr*)&cin,&taille)))
 	{
 		pthread_t th;
+		argTh *arg;
 		arg = (argTh *)malloc(sizeof(argTh));
 		arg->socket = cfd;
-		if (pthread_create (&th,NULL,codeFrere,(void *)&arg) < 0)
+		if (pthread_create (&th,NULL,codeFrere,arg) < 0)
 		{
 			fprintf(stderr,"pthread_create\n");
 			exit(1);
